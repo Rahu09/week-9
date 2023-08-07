80720e2e2526a48fe1871d61eeb29decada79711
Rahul Tiwari <tiwarirahul0809@gmail.com>
2023-08-08 03:11:22 +0530

    notes updated

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,15 +7,15 @@ const authenticateJwt = (req, res, next) => {
     const token = authHeader.split(' ')[1];
     jwt.verify(token, SECRET, (err, user) => {
       if (err) {
-        return res.status(403).json({
-          msz:"you are maybe unauthorized"
-        });
+        return res.sendStatus(403);
       }
       req.user = user;
       next();
     });
   } else {
-    res.sendStatus(401);
+    res.sendStatus(401).json({
+      msz:"you are maybe unauthorized"
+    });
   }
 };
 
